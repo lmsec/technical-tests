408961e1f90747be45b1d90b09bf24f60666a770
Lise MIRA <LSecurity@protonmail.ch>
2020-09-24 13:35:03 +0200

    react tests: ReactLevel1 as a class

--- react-tests/src/components/react-level-1.js
+++ react-tests/src/components/react-level-1.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-export function ReactLevel1(props) {
-    return <h1>Hello, it works !</h1>;
+export class ReactLevel1 extends React.Component {
+    render() {
+        return (
+            <h1>Hello, it works !</h1>
+        );
+    }
 }
